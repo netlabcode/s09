cae54871ce7b774b44269b1556047ca48dcc9e22
APresekal <presekal@ui.ac.id>
2021-04-06 08:58:23 +0200

    Add files via upload

--- a-mn-9-18-23.py
+++ a-mn-9-18-23.py
@@ -276,6 +276,12 @@ def emptyNet():
     info( net[ 's18m1' ].cmd( 'python3.6 as18gdb.py &amp' ) )
     info( net[ 's23m1' ].cmd( 'python3.6 as23gdb.py &amp' ) )
 
+    time.sleep(2)
+
+    info( net[ 's09m1' ].cmd( 'python3.6 as09gcc.py &amp' ) )
+    info( net[ 's18m1' ].cmd( 'python3.6 as18gcc.py &amp' ) )
+    info( net[ 's23m1' ].cmd( 'python3.6 as23gcc.py &amp' ) )
+
 
 
 
